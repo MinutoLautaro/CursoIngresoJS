49453ebd27d3e8e92bfaa86a9eb68c2e0be9c418
alumno <alumno@utnfra.com.ar>
2020-02-04 20:11:30 -0300

    ejercicios

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,17 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
-}
+    var nombre;
 
+     var edad;
+   
+    nombre = document.getElementById("elNombre").value;
+
+    edad = document.getElementById("laEdad").value;
+
+    console.log(nombre);
+
+    console.log(edad);
+    
+    alert("usted se llama " + nombre + " y su edad es " + edad);
+}
