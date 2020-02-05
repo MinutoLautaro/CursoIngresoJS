b984171edf3f9692d2be4d65459f39d373764a8d
MinutoLautaro <minutolautaro@hotmail.com>
2020-02-05 14:30:01 -0300

    ejercicio1if

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
+var condicion 
+
+
+edad = document.getElementById("edad").value;
+condicion = edad == 15;
+
+if(condicion)
+{
+alert("niña bonita");
+}
+else
+{
+    alert("niña fea");
+}
 
 
 
