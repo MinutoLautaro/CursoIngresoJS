7a8a945b5efa0312e1641282095929efb6f3ccc9
MinutoLautaro <minutolautaro@hotmail.com>
2020-02-05 12:42:41 -0300

    ejecicio10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var resultado;
+    var total;
+
+    importe = document.getElementById("importe").value;
+    total = document.getElementById("importe").value;
+
+    resultado = parseInt(resultado);
+    total = parseInt(total);
+
+    resultado = (importe * 25) / 100
+    total = total - resultado
+
+    document.getElementById("resultado").value = total
 }
