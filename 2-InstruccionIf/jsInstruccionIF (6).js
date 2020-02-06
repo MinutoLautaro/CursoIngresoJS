44ac75a8c3729d9d272b1fd1dc26803a792da2c5
MinutoLautaro <minutolautaro@hotmail.com>
2020-02-06 12:56:54 -0300

    ejercicioIf6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,33 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    var condicionUno;
+    var condicionDos;
+    var adolescente;
+    var menor;
+    var mayor;
 
+    edad = document.getElementById("edad").value;
+    condicionUno = edad >= 13;
+    condicionDos = edad <= 17;
+    menor = edad < 13;
+    mayor = edad > 17;
+    adolescente = condicionUno && condicionDos;
+
+    if(adolescente)
+    
+    alert("Usted es adolescente")
+    
+    else if(menor)
+    {
+        alert("Usted es un niño");
+    }
+    else
+    {
+    alert("Usted es mayor de edad");
+    }
+    
 
 
 
