d97aca6cf3f4218e616f7bb621bdb6d41049e992
alumno <alumno@utnfra.com.ar>
2020-02-18 22:09:53 -0300

    lrpum

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,15 +1,23 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador = 0;
+	var contadorPositivos = 0;
+	var contadorNegativos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
 	//declarar contadores y variables 
-	
+	var numero;
+	var positivos;
 	var respuesta="si";
 
-	while(respuesta!="no")
+	while(confirm("Desea ingresar un numero"))
 	{
-		
-	
+		numero = prompt("ingrese un numero");
+		parseInt(numero);
+		if(numero >= 0);
+		contadorPositivos++;
+		positivo = positivo + numero;
 	}
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,46 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numero;
+	var maximo;
+	var minimo
+	var primeraVez;
 	var respuesta='si';
 
-	while(respuesta!='no')
+
+
+	/*while(respuesta!='no')
 	{
+		numero = prompt("ingrese un numero");
+		numero = parseInt(numero);
+
+		while(isNaN(numero)){
+			numero = prompt("ingrese un numero valido");
+			numero = parseInt(numero)
+		}*/
+do{
+	numero = prompt("ingrese un numero");
+	numero = parseInt(numero);
+}while(isNaN(numero));
+		if(primeraVez){
+			primeraVez = false;
+
+			maximo = numero
+			minimo = numero
+		}
+		else {
+			if(numero > maximo){
+				maximo = numero
+		}
+			if(numero < minimo){
+				minimo = numero
+			}
 		
+		respuesta = confirm("¿desea continuar?");
 	
 	}
 
-
-
-
-}//FIN DE LA FUNCIÓN
+		document.getElementById("maximo").value = maximo;
+		document.getElementById("minimo").value = minimo;
+}
+//FIN DE LA FUNCIÓN
