445176d77170742ca48a365d24f48faca8a0bf39
alumno <alumno@utnfra.com.ar>
2020-03-03 21:14:21 -0300

    quedolordecabeza

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,79 @@
-function mostrar()
-{
+function mostrar() {
+    var edad;
+    var edadJoven = 0;
+    var edadVieja = 0;
+    var edadMujeres = 0;
+    var pasaje;
+    var pasajeViejo;
+    var sexo;
+    var sexoViejo;
+    var nombre;
+    var nombreJoven;
+    var contador = 0;
+    var contadorMujeres = 0;
+    var contadorMujeresUN = 0;
+    var promedioMujeres = 0;
+    while (confirm("¿Desea ingresar datos?")) {
+        do {
+            edad = prompt("Ingrese su edad");
+            edad = parseInt(edad);
 
+        } while (edad < 18);
+        contador++
+        do {
+            sexo = prompt("ingrese sexo");
+
+        } while (sexo != "f" && sexo != "m");
+        do {
+            pasaje = prompt("ingrese tipo de pasaje");
+
+        } while (pasaje != "urbano" && pasaje != "nacional" && pasaje != "internacional");
+
+        nombre = prompt("Ingrese su nombre");
+        //Fin toma de datos
+        if (contador == 1 && sexo == "m" && pasaje == "nacional") {
+            edadJoven = edad;
+            nombreJoven = nombre;
+        }
+        if (contador != 1 && edad < edadJoven && sexo == "m" && pasaje == "nacional") {
+            edadJoven = edad;
+            nombreJoven = nombre;
+        }
+        if (contador == 1) {
+            sexoViejo = sexo;
+            edadVieja = edad;
+            pasajeViejo = pasaje;
+        }
+        if (contador != 1 && edad > edadVieja) {
+            sexoViejo = sexo;
+            edadVieja = edad;
+            pasajeViejo = pasaje;
+        }
+        if (sexo == "f" && pasaje == ("nacional" || pasaje == "urbano")) {
+            contadorMujeresUN++
+        }
+        if (sexo == "f") {
+            contadorMujeres++
+            edadMujeres = edadMujeres + edad;
+        }
+        //ABC
+    }
+    
+    if (edadJoven != 0) {
+        alert(nombreJoven + " es el hombre mas joven con pasaje nacional");
+    }
+    else {
+        alert("no hay ningun hombre joven con pasaje nacional")
+    }
+    if (edadVieja != 0) {
+        alert("El pasajero mas viejo es " + sexoViejo + " y su pasaje es " + pasajeViejo);
+    }
+    else {
+        alert("no se han registrado pasajeros");
+    }
+    alert("Cant. mujeres con pasajes nacional o urbano" + contadorMujeresUN);
+promedioMujeres =  edadMujeres / contadorMujeres   
+    if (edadMujeres != 0){
+        alert("el promedio de edad de las pasajeras mujeres es " + promedioMujeres);
+    }
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,38 @@
 
 function mostrar()
 {
+    var edad;
+    var pasaje;
+    var sexo;
+    var nombre;
+    var edadJoven = 100;
+ alert("hola");
+  while(confirm("¿Desea ingresar datos?")){
+    do{
+       
+        edad = prompt("Ingrese su edad");
+       
+        edad = parseInt(edad); 
+    }while(edad < 18);
 
+
+     do{
+       
+         sexo = prompt("ingrese sexo");
+
+        }while (sexo != "f" && sexo != "m");
+     do{
+         
+         pasaje = prompt("ingrese tipo de pasaje");
+
+     }while(pasaje != "urbano" && pasaje != "nacional" && pasaje != "internacional");
+     
+      nombre = prompt("Ingrese su nombre");
+
+      if(edad < edadJoven && sexo == "m" && pasaje == "nacional"){
+          edadJoven = edad
+         alert(nombre + " con " + edadJoven + " años es el hombre mas joven con el pasaje tipo " + pasaje );
+      }
+  }
+    
 }
